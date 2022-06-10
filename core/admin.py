7d12a78686ac87567d438c3e113004d9ca1e795f
bak <bak4017@gmail.com>
2022-06-10 20:28:57 +0900

    do it 장고 521 페이지까지 클론한 것

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import PopularMovie, Profile, Movie, CustomUser
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(CustomUser)
-admin.site.register(Movie)
-admin.site.register(PopularMovie)
